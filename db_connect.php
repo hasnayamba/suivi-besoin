3dc2e73448bdd7a67e75a633b5a52198149238fe
hasnayamba <yambahassana@gmail.com>
2026-03-09 12:07:02 +0100

    azure simple

--- db_connect.php
+++ db_connect.php
@@ -5,20 +5,18 @@ $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
 
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
-];
-
 try {
 
-    $pdo = new PDO(
-        "mysql:host=$host;port=3306;dbname=$db;charset=utf8mb4;sslmode=require",
-        $user,
-        $pass,
-        $options
-    );
+    $dsn = "mysql:host=$host;port=3306;dbname=$db;charset=utf8mb4";
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        PDO::MYSQL_ATTR_SSL_CA => true
+    ];
+
+    $pdo = new PDO($dsn, $user, $pass, $options);
 
 } catch (PDOException $e) {
 
